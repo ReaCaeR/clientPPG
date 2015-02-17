ff854a4059f9e0469674789c1787e8ce54c4b463
Cekkins <cekkins@live.it>
2015-02-17 21:39:01 +0100

    Service con Admin

--- PPGService.java
+++ PPGService.java
@@ -6,75 +6,106 @@ import javax.jws.WebParam;
 import java.sql.*;
 
 class Database{
-	private Connection con;
-	private PreparedStatement pst;
-	private ResultSet rs;
-        static Database db;
+    
+    private Connection con;
+    private PreparedStatement pst;
+    private ResultSet rs;
+    static Database db;
         
-        public static Database getDatabaseHandler()
-        {
-            if(db == null)
-            {
-                db = new Database();
-            }
-            return db;
-        }
+     public static Database getDatabaseHandler(){
+        if(db == null)
+            db = new Database();
+        return db;
+    }
         
-	private Database(){
-		try{
-                    Class.forName("com.mysql.jdbc.Driver");
-                    con=DriverManager.getConnection("jdbc:mysql://localhost:3306/paintpg_db","paintpgadm","paintpg");
-		}
-		catch (Exception e) {
-                    System.out.println(e);
-		}
+    private Database(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/paintpg_db","paintpgadm","paintpg");
 	}
-	//LOGIN/REGISTRATION METHODS	
-	protected String registrationRequest(String usr, String pwd, String mot, String c1, String c2){
-            try {
-                String query="SELECT `user_authentication`.`username` FROM `paintpg_db`.`user_authentication` WHERE `username`=?";
-                pst=con.prepareStatement(query);
-                pst.setString(1, usr);
-                rs=pst.executeQuery();
-                if(rs.next())
-                return "Nome utente già in uso!";
-                pst.close();
-                query="INSERT INTO `paintpg_db`.`user_authentication` (`user_id`, `username`, `password`, `motto`, `colore1`, `colore2`)" 
-                      + "VALUES (NULL, ?, ?, ?, ?, ?)";
-                pst=con.prepareStatement(query);
-                pst.setString(1, usr);
-                pst.setString(2, pwd);
-                pst.setString(3, mot);
-                pst.setString(4, c1);
-                pst.setString(5, c2);
-                pst.executeUpdate();
-            } catch (Exception e) {
-                return "Errore! " + e;
-            }
-            return "Registrazione effettuata con successo!";
-        }
-        
-        protected int checkLogin(String usr,String pwd){
-            try {
-		String query="SELECT * FROM user_authentication WHERE username=? AND password=?";
-		pst=con.prepareStatement(query);
-		pst.setString(1, usr);
-		pst.setString(2, pwd);
-		rs=pst.executeQuery();
-		if(rs.next())
-                {
-                    int id = Integer.parseInt(rs.getString("user_id"));
-                    return id;
-                }
-                else
-                {
-                    return -1;
-                }
-            } catch (Exception e) {
-		return -1;
-            }
+	catch (Exception e) {
+            System.out.println(e);
 	}
+    }
+    
+    //LOGIN/REGISTRATION METHODS	
+    protected String registrationRequest(String usr, String pwd, String mot, String c1, String c2){
+        try {
+            String query="SELECT `user_authentication`.`username` FROM `paintpg_db`.`user_authentication` WHERE `username`=?";
+            pst=con.prepareStatement(query);
+            pst.setString(1, usr);
+            rs=pst.executeQuery();
+            if(rs.next())
+            return "Nome utente già in uso!";
+            pst.close();
+            query="INSERT INTO `paintpg_db`.`user_authentication` (`user_id`, `username`, `password`, `motto`, `colore1`, `colore2`)" 
+                  + "VALUES (NULL, ?, ?, ?, ?, ?)";
+            pst=con.prepareStatement(query);
+            pst.setString(1, usr);
+            pst.setString(2, pwd);
+            pst.setString(3, mot);
+            pst.setString(4, c1);
+            pst.setString(5, c2);
+            pst.executeUpdate();
+        } catch (Exception e) {
+            return "Errore! " + e;
+        }
+        return "Registrazione effettuata con successo!";
+    }
         
+    protected int checkLogin(String usr,String pwd){
+        try {
+            String query="SELECT * FROM user_authentication WHERE username=? AND password=?";
+            pst=con.prepareStatement(query);
+            pst.setString(1, usr);
+            pst.setString(2, pwd);
+            rs=pst.executeQuery();
+            if(rs.next()){
+                int id = Integer.parseInt(rs.getString("user_id"));
+                return id;
+            }
+            else{
+                return -1;
+            }
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+    
+    //ADMIN METHODS
+    protected int isAdmin(int userID){
+        try {
+            String query="SELECT `admin_level` FROM `admin` WHERE `user_id` = ?";
+            pst=con.prepareStatement(query);
+            pst.setInt(1, userID);
+            rs=pst.executeQuery();
+            if(rs.next())
+		return rs.getInt("admin_level");                 
+            else 
+                return -1;
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+        
+    protected void setAdmin(int userID){
+        try{
+            String query="INSERT INTO `admin`(`user_id`, `admin_level`) VALUES (?, ?)";
+            pst=con.prepareStatement(query);
+            pst.setInt(1, userID);
+            pst.setInt(2, 2);
+            pst.executeUpdate();
+        } catch (Exception e) {}
+    }
+        
+    protected void rmvAdmin(int userID){
+        try{
+            String query="DELETE FROM `admin` WHERE `user_id` = ?";
+            pst=con.prepareStatement(query);
+            pst.setInt(1, userID);
+            pst.executeUpdate();
+        } catch (Exception e) {}
+    }       
 }
 
 @WebService(serviceName = "PaintPGService")
@@ -98,6 +129,22 @@ public class PPGService{
     public int loginRequest(    @WebParam(name = "username") String username,
                                 @WebParam(name = "password") String password){
         return db.checkLogin(username,password);
+    }
+    
+    //ADMIN WEBMETHODS
+    @WebMethod(operationName = "isAdmin")
+    public int isAdmin(@WebParam(name ="userID") int userID){
+        return db.isAdmin(userID);
+    }
+    
+    @WebMethod(operationName = "setAdmin")
+    public void setAdmin(@WebParam(name ="userID") int userID){
+        db.setAdmin(userID);
+    }
+    
+    @WebMethod(operationName = "rmvAdmin")
+    public void rmvAdmin(@WebParam(name ="userID") int userID){
+        db.rmvAdmin(userID);
     }
     
     @WebMethod(operationName = "DEBUGSendDate")
