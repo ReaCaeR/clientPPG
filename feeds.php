f84339b41a0a729041f6ea74e393527c0d2c36f7
ReaCaer <caerdf@hotmail.com>
2015-02-24 18:39:51 +0100

    test css feeds

---
+++ feeds.php
@@ -0,0 +1,66 @@
+<div class='feeds'>
+	<div class='feed_wrapper'>
+		<!-- TEAM LOOKING FOR CHALLENGER -->
+		<div class='feed'>
+			<div class='challenge_pending'>
+				<div class='challenge_date'>
+					[DATA_SFIDA]
+				</div>
+				<div class='team_looking_for_challenger'>
+					[NOME_TEAM_CERCA_SFIDANTE]
+					<br/>
+					[MOTTO_TEAM_CERCA_SFIDANTE]
+					<br/>
+					[ACCETTA_SFIDA]
+				</div>
+			</div>
+		</div>
+
+		<!-- UPCOMING CHALLENGE -->
+		<div class='feed'>
+			<div class='challenge_accepted'>
+				<div class='challenge_date'>
+					[DATA_SFIDA]
+				</div>
+				<div class='teams'>
+					<div class='team_1'>
+						[NOME_TEAM_1]
+						<br/>
+						[MOTTO_TEAM_1]
+					</div>
+					<div class='team_2'>
+						[NOME_TEAM_2]
+						<br/>
+						[MOTTO_TEAM_2]
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- CHALLENGE RESULTS -->
+		<div class='feed'>
+			<div class='challenge_ended'>
+				<div class='challenge_date'>
+					[DATA_SFIDA]
+				</div>
+				<div class='teams'>
+					<div class='team_1'>
+						[NOME_TEAM_1]
+						<br/>
+						[MOTTO_TEAM_1]
+						<br/>
+						[RISULTATI_TEAM_1]
+					</div>
+					<div class='team_2'>
+						[NOME_TEAM_2]
+						<br/>
+						[MOTTO_TEAM_2]
+						<br/>
+						[RISULTATI_TEAM_2]
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</div>
+</div>
--- index.php
+++ index.php
@@ -9,8 +9,18 @@
     <div class='head'><?php include("menu_top.php"); ?></div>
     <div class='bodywrap'>
         <div class='left'><?php echo isset($_SESSION['user_id']) ? include("profile.php") : 'NOT LOGGED IN'; ?></div>
-        <div class='right'><?php if (isset($_SESSION['user_id'])) include("send_match.php");?></div>
-        <div class='center'><?php if (!isset($_SESSION["user_id"])) include("register.php"); else include("send_post.php");?></div>
+        <div class='right'><?php if(isset($_SESSION['user_id'])) include("send_match.php");?></div>
+        <div class='center'>
+        	<?php 
+        		if (!isset($_SESSION["user_id"])){
+        			include("register.php");
+        		}
+        		else{
+        			//include("send_post.php");
+        			include("feeds.php");
+        		}
+    		?>
+        </div>
     </div>
  </div>
 </body>
